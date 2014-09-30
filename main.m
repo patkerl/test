3335ddde3a45b73e324731962496475b2faea937
patkerl <patkerl.jobs@orange.fr>
2014-09-30 11:49:07 +0200

    maj date

---
+++ main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Hello_World
+//
+//  Created by PATRICK KERLAU on 29/12/10.
+//  Copyright 2010 none. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
